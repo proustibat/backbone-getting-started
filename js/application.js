6efc2df2ac3854dab3a6becd729d666101ae8daf
Jennifer Proust <jennifer.proust@gmail.com>
2014-10-14 18:54:08 +0200

    Templates: inline template

--- js/application.js
+++ js/application.js
@@ -41,23 +41,44 @@ var AnimalView = Backbone.View.extend({
 });
 
 
-// PART 1 : MODEL
-// var dog = new Animal();
+// TEMPLATES: inline templates
+var AnimalView2 = Backbone.View.extend({
+  tagName: 'li', // defaults to div if not specified
+  className: 'animal', // optional, can also set multiple like 'animal dog'
+  id: 'dogs', // also optional
+  events: {
+    'click': 'alertTest',
+    'click .edit': 'editAnimal',
+    'click .delete': 'deleteAnimal'
+  },
+  template: _.template('The dog called <%= name %> is <%= color %> and says <%= sound %>.'), // inline template
+  // initialize function watches for changes in a model and executes code as soon as a new model instance is created
+  initialize: function() {
+    this.on('change', function() {
+      console.log('Something has changed');
+    });
+    this.render(); // render is an optional function that defines the logic for rendering a template
+  },
+  render: function() {
+    // the below line represents the code prior to adding the template
+    this.$el.html(this.template(this.model.toJSON())); // calls the template
+  }
+});
 
+
+
+// PART 1: MODEL
+// var dog = new Animal();
 // console.log(dog);
 // console.log(dog.get("name"));
-
 // dog.set("name", "Dog 1");
 // console.log(dog.get("name"));
-
 // dog.set({
 //   name: "Dog 1",
 //   color: "brown",
 //   sound: "arf"
 // });
 // console.log(dog.toJSON());
-
-
 // var dog2 = new Animal({
 //   name: 'Dog 2',
 //   color: 'brown',
@@ -69,9 +90,21 @@ var AnimalView = Backbone.View.extend({
 // console.log(dog2.toJSON());
 
 
-// PART 2 : VIEWS
+
+// PART 2: VIEWS
+// var dog = new Animal();
+// var dogView = new AnimalView({
+//   model: dog
+// });
+// console.log(dogView);
+// console.log(dogView.el);
+// $(document.body).html(dogView.el);
+
+
+
+// PART 3: TEMPLATES
 var dog = new Animal();
-var dogView = new AnimalView({
+var dogView = new AnimalView2({
   model: dog
 });
 console.log(dogView);
