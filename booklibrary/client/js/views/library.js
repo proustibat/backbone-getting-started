c51d5e0a965ca6baefd6e4b7489afad2a54c614e
Jennifer Proust <jennifer.proust@gmail.com>
2014-10-27 18:47:39 +0100

    >>> wip

--- booklibrary/client/js/views/library.js
+++ booklibrary/client/js/views/library.js
@@ -10,12 +10,15 @@ app.LibraryView = Backbone.View.extend({
 		this.render();
 	},
 
+	// render library by rendering each book in its collection
 	render: function() {
 		this.collection.each(function(item) {
 			this.renderBook(item);
 		}, this);
 	},
 
+	// render a book by creating a BookView and appending the
+    // element it renders to the library's element
 	renderBook: function(item) {
 		var bookView = new app.BookView({
 			model: item
